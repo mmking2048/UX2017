31dd15561bacced50799777a8cb7d9b98ed57f5f
Michelle King <mmking2048@gmail.com>
2017-10-09 23:37:09 -0500

    Removed unused usings

--- UX2017/Models/ProfileAndFinancialData/FinancialRatio.cs
+++ UX2017/Models/ProfileAndFinancialData/FinancialRatio.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace UX2017.Models.ProfileAndFinancialData
+﻿namespace UX2017.Models.ProfileAndFinancialData
 {
     public class FinancialRatio
     {
