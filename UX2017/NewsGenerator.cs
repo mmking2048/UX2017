e25d6408deccee7812253a4022a756f70dca87ad
Michelle King <mmking2048@gmail.com>
2017-10-09 23:33:55 -0500

    Uncommitted junk

---
+++ UX2017/NewsGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UX2017
+{
+    public static class NewsGenerator
+    {
+        public static string GetEarningsNews()
+        {
+            return "Beep";
+        }
+    }
+}
