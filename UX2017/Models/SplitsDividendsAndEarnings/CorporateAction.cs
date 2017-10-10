8f64ddfd82d69d3686f62583a0b6ded979c176bd
Michelle King <mmking2048@gmail.com>
2017-10-09 23:32:58 -0500

    Added Splits, Dividends, and Earnings API DTOs

---
+++ UX2017/Models/SplitsDividendsAndEarnings/CorporateAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UX2017.Models.SplitsDividendsAndEarnings
+{
+    public class CorporateAction
+    {
+        public string Symbol { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventType { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ UX2017/Models/SplitsDividendsAndEarnings/EarningsEstimate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UX2017.Models.SplitsDividendsAndEarnings
+{
+    public class EarningsEstimate
+    {
+        public string Symbol { get; set; }
+        public string SymbolName { get; set; }
+        public string Period { get; set; }
+        public double AverageEstimate { get; set; }
+        public int? NumAnalysts { get; set; }
+        public double? HighEstimate { get; set; }
+        public double? LowEstimate { get; set; }
+        public double? PriorYear { get; set; }
+        public double? GrowthRateEstimate { get; set; }
+        public DateTime? CurrentQtrExpectedReportDate { get; set; }
+        public string ExpectedEarningsStatus { get; set; }
+        public string ExpectedEarningsSource { get; set; }
+        public string DividendType { get; set; }
+        public double? IndicatedAnnualDvnd { get; set; }
+        public string DividendStatus { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public DateTime? RecordDate { get; set; }
+        public DateTime? DeclarationDate { get; set; }
+        public DateTime? ExDividendDate { get; set; }
+        public double? DividendRate { get; set; }
+    }
+}
