694e101df5342eeb6e2b2cd7bd16e2df3f1ab60c
Michelle King <mmking2048@gmail.com>
2017-10-17 21:51:46 -0500

    Made more logical controllers

--- UX2017/Controllers/HomeController.cs
+++ UX2017/Controllers/HomeController.cs
@@ -4,11 +4,9 @@ namespace UX2017.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly INewsGenerator _generator = new NewsGenerator();
 
         public ActionResult Index()
         {
-            ViewBag.News = _generator.GetEarningsNews();
             return View();
         }
     }
---
+++ UX2017/Controllers/NewsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace UX2017.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsGenerator _generator = new NewsGenerator();
+
+        public ActionResult Index()
+        {
+            ViewBag.News = _generator.GetEarningsNews();
+            return View("News");
+        }
+    }
+}
