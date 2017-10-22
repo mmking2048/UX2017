880f40b85fe3639187ad39c3481a5652fd55c036
Michelle King <mmking2048@gmail.com>
2017-10-22 17:39:21 -0500

    Added table for key data

--- UX2017/Controllers/ProfileController.cs
+++ UX2017/Controllers/ProfileController.cs
@@ -14,7 +14,15 @@ namespace UX2017.Controllers
         public async Task<ActionResult> GetProfile(string symbol)
         {
             ViewBag.Profile = await _barchartClient.GetProfiles(symbol);
-            ViewBag.Quote = await _barchartClient.GetQuote(symbol);
+            ViewBag.Quote = (await _barchartClient.GetQuote(new[] {symbol}, new[] {"R"},
+                    new[] {"previousClose", "bid", "ask", "fiftyTwoWkLow", "fiftyTwoWkHigh", "avgVolume"}))
+                .ElementAt(0);
+            ViewBag.FinancialHighlight = (await _barchartClient.GetFinancialHighlights(new[] {symbol},
+                    new[] {"beta", "peRatio", "ttmEPS"}))
+                .ElementAt(0);
+            ViewBag.Estimate = (await _barchartClient.GetEarningsEstimates(new[] {symbol},
+                    new[] {"exDividendDate", "dividendRate"}))
+                .ElementAt(0);
             ViewBag.Chart = await _barchartClient.GetChart(symbol);
             ViewBag.NewsArticles = (await _barchartClient.GetNews(symbol))
                 .Take(10)
