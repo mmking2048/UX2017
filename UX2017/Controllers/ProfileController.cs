9e5277eed211de0af6dcd267c1f62a70db661c29
Michelle King <mmking2048@gmail.com>
2017-10-23 22:13:48 -0500

    Finished earnings infpo

--- UX2017/Controllers/ProfileController.cs
+++ UX2017/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using UX2017.Models;
+using UX2017.Models.SplitsDividendsAndEarnings;
 
 namespace UX2017.Controllers
 {
@@ -28,7 +29,30 @@ namespace UX2017.Controllers
                 .Take(10)
                 .Select(n => new NewsArticle(n.NewsID, n.Headline, n.Preview));
 
+            var earnings = await _barchartClient.GetCorporateActions(new[] {symbol}, null, null, EventType.earnings, 5);
+            var dividends = await _barchartClient.GetCorporateActions(new[] { symbol }, null, null, EventType.dividend, 5);
+
+            if (dividends == null)
+            {
+                ViewBag.Earnings = earnings.Select(e => new EarningsData {Earnings = e});
+            }
+            else if (earnings == null)
+            {
+                ViewBag.Earnings = dividends.Select(d => new EarningsData {Dividends = d});
+            }
+            else
+            {
+                ViewBag.Earnings = earnings.Join(dividends, e => e.EventDate.Month, d => d.EventDate.Month,
+                    (e, d) => new EarningsData {Earnings = e, Dividends = d});
+            }
+
             return View("Profile");
         }
+    }
+
+    public class EarningsData
+    {
+        public CorporateAction Earnings { get; set; }
+        public CorporateAction Dividends { get; set; }
     }
 }
